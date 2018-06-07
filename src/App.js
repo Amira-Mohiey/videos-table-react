6f3c14c44217ca37ab90cd5d3e2533a1b97f6a81
amira-mohiey <amira.mohiey@cartologic.com>
2018-06-07 22:03:18 +0200

    add filters

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import VideoTable from './components/videoTable'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <VideoTable/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/videoTable.jsx
@@ -0,0 +1,172 @@
+import React, { Component } from 'react';
+import { tableService } from '../services/tableService'
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import ArrowUpIcon from "@material-ui/icons/ArrowDropUp";
+import ArrowDownIcon from "@material-ui/icons/ArrowDropDown";
+import Button from "@material-ui/core/Button";
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+export default class VideoTable extends Component {
+  state = {
+    videos: null,
+    viewsOrder: 'asc',
+    likesOrder: 'asc',
+    shareOrder: 'asc',
+  }
+  componentDidMount() {
+    var videos = tableService()
+    this.setState({ videos, filteredVideos: videos })
+  }
+
+  filter = name => event => {
+    var filteredVideos = this.state.videos.filter(video => {
+      return video[name].toLowerCase().includes(event.target.value.toLowerCase())
+    });
+    this.setState({ filteredVideos })
+  }
+
+
+  handleSortClick = (name, order) => event => {
+    switch (name) {
+      case "views":
+        this.state.viewsOrder == 'asc' ? this.setState({ viewsOrder: "desc" }) : this.setState({ viewsOrder: "asc" })
+        break;
+      case "shares":
+        this.state.shareOrder == 'asc' ? this.setState({ shareOrder: "desc" }) : this.setState({ shareOrder: "asc" })
+        break;
+      case "likes":
+        this.state.likesOrder == 'asc' ? this.setState({ likesOrder: "desc" }) : this.setState({ likesOrder: "asc" })
+        break;
+    }
+    this.sort(name, order)
+  }
+
+  sort = (name, order) => {
+    var videos = this.state.filteredVideos
+    order === 'desc'
+      ? videos.sort((a, b) => (b[name] < a[name] ? -1 : 1))
+      : videos.sort((a, b) => (a[name] < b[name] ? -1 : 1));
+
+    this.setState({ videos })
+
+  }
+
+  renderTitleFilter = () => {
+    return (
+      <TextField
+       
+        className="filter"
+        id="input-with-icon-textfield"
+        label="filter by video title"
+        onChange={this.filter("title")}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    )
+  }
+  renderPageFilter = () => {
+    return (
+      <TextField
+        className="filter"
+        onChange={this.filter("parent_name")}
+        id="input-with-icon-textfield"
+        label="filter by page name"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    )
+  }
+  renderTable() {
+    return (
+      <Paper className="table"  >
+        {this.renderTitleFilter()}
+        {this.renderPageFilter()}
+        {this.state.filteredVideos && <Table className="table"  >
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell >Page Name</TableCell>
+              <TableCell >Publish date</TableCell>
+              <TableCell >
+                <TableSortLabel
+                  active={true}
+                  direction={this.state.viewsOrder}
+                  onClick={this.handleSortClick("views", this.state.viewsOrder)}
+                >
+                  Views
+                  </TableSortLabel>
+              </TableCell>
+              <TableCell >
+                <TableSortLabel
+                  active={true}
+                  direction={this.state.likesOrder}
+                  onClick={this.handleSortClick("likes", this.state.likesOrder)}
+                >
+                  likes
+                  </TableSortLabel>
+              </TableCell>
+              <TableCell >
+                <TableSortLabel
+                  active={true}
+                  direction={this.state.shareOrder}
+                  onClick={this.handleSortClick("shares", this.state.shareOrder)}
+                >
+                  Shares
+                </TableSortLabel>
+              </TableCell>
+              <TableCell >Comments</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.filteredVideos.map((video, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell component="th" scope="row">
+                    {video.title}
+                  </TableCell>
+
+                  <TableCell >{video.parent_name}</TableCell>
+                  <TableCell >{video.published}</TableCell>
+                  <TableCell >{video.views}</TableCell>
+                  <TableCell >{video.likes}</TableCell>
+                  <TableCell >{video.comments}</TableCell>
+                  <TableCell >{video.shares}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>}
+        { this.state.filteredVideos && !this.state.filteredVideos.length && <p className="result" >No result found</p>}
+      </Paper>
+    )
+  }
+  render() {
+    return (
+      <div>
+        {this.renderTable()}
+      </div>
+
+    );
+  }
+}
+
